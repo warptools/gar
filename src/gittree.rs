84c8b741ccead9134e3cf0a02a1ad2a0b727c717
Eric Myhre <hash@exultant.us>
2024-08-23 16:27:41 +0200

    fixtures for dirs.  they aren't matching; unclear why.

--- src/gittree.rs
+++ src/gittree.rs
@@ -1,14 +1,21 @@
 use bytes::BufMut;
 use bytes::BytesMut;
 use sha2::Digest;
+use std::fmt::Debug;
 use std::fs;
 use std::io;
 use std::os::unix::fs::MetadataExt;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 struct Hash([u8; 32]);
+
+impl Debug for Hash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Hash").field(&hex::encode(&self.0)).finish()
+    }
+}
 
 impl Hash {
     fn from_hex<S: AsRef<[u8]>>(hex: S) -> Result<Self, hex::FromHexError> {
@@ -56,7 +63,19 @@ mod tests {
     }
 
     #[rstest]
+    // Just a plain file.
     #[case("fixtures/alpha/a_file", Hash::from_hex("2909489adcb095aa795a9a7e6d92db735d0a0ced0782c43496675bdb7beec3ce").expect(""))]
+    #[case("fixtures/alpha/a_dir/other_file", Hash::from_hex("8431d03990244d0bffa3dfecdd7a67d0bca2f5e999bff04469cde93cc2365d96").expect(""))]
+    #[case("fixtures/alpha/a_dir/more_files", Hash::from_hex("4698ba4d7c51602d6a50e4fb6e150e2e06d625ba5874cde627bc6dfc357a23db").expect(""))]
+    #[case("fixtures/alpha/a_dir/deeper/samefile", Hash::from_hex("4698ba4d7c51602d6a50e4fb6e150e2e06d625ba5874cde627bc6dfc357a23db").expect(""))]
+    // Dir with one file.  (Sorting thus can't be the problem, if this fixture fails.)
+    #[case("fixtures/alpha/a_dir/deeper", Hash::from_hex("9897054d9f01c666ac1371d3e0a022a67b5df59ddb1608e8165a3b1fa22da706").expect(""))]
+    // Dir with files and subdirs.
+    #[case("fixtures/alpha/a_dir", Hash::from_hex("e1896fb25dd721b447c52e40267a90405ebc41aaa2c7143e9cf58cf5c8421cde").expect(""))]
+    // A wild symlink appears!
+    #[case("fixtures/alpha/a_symlink", Hash::from_hex("45a01848912e900ef582a23ef763c77ddb2d955bea7756072fb056f43534fca8").expect(""))]
+    // Dir with multiple files (sorting matters), symlinks, and subdirs (including recursively).
+    #[case("fixtures/alpha", Hash::from_hex("9024a7f8afa43db06ff2b50d9ac9c21b791bee49d8092d3f14f1e433bfd927fa").expect(""))]
     fn test_hash_of_path(#[case] path: String, #[case] expected: Hash) {
         assert_eq!(expected, hash_of_path(path).expect("no io errors"))
     }
@@ -98,6 +117,8 @@ fn hash_of_path<P: AsRef<Path>>(path: P) -> Result<Hash, io::Error> {
                 buf.put(&b"120000 "[..])
             } else if ft.is_dir() {
                 buf.put(&b"40000 "[..]) // This certainly looks like a typo, doesn't it!  But, indeed... this is exactly how git encodes this.
+            } else {
+                panic!("unknown file type")
             }
             // Now the name (and a terminating delimiter).
             buf.put::<&[u8]>(ent.file_name().as_os_str().as_encoded_bytes());
@@ -106,6 +127,8 @@ fn hash_of_path<P: AsRef<Path>>(path: P) -> Result<Hash, io::Error> {
             buf.put(&hash.0[..])
             // Somewhat shockingly, there's no further delimiter here.  The hash length is necessarily hardcoded by this absense.
         }
+
+        eprintln!("debug: tree buff is -->{buf:?}<--");
 
         // Ultimately: hash the preamble and feed the buffer and finalize.
         let mut hasher = sha2::Sha256::new();
